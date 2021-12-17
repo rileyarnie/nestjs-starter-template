ec5f0e8746fa808f8d5e83153b7061d7758fb5ae
Arnold Kibira <arnold.ril3y@gmail.com>
2021-12-17 15:14:16 +0300

    Feat - Set up mysql database connection with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
